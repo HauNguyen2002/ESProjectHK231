dc1ce25519431ed1374914d7a7efe433dec79fe5
HauNguyen2002 <93597645+HauNguyen2002@users.noreply.github.com>
2024-01-03 00:54:49 +0700

    First Commit

---
+++ Gesture_transmitter.ino
@@ -0,0 +1,102 @@
+#include <RevEng_PAJ7620.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+RF24 radio(9,10); //Create a transceiver object that has CE and CSE pins at 9 and 10
+
+RevEng_PAJ7620 sensor = RevEng_PAJ7620(); //Create a sensor oprating object
+
+const byte addr[6]="12345"; //Tranmission address
+char SentMessage[1]="";// Initialize a char array for storing data
+
+//----------Detect gestures and put the result in the char array--------------------
+void gesMode(){
+  Gesture gesture=sensor.readGesture();
+  int obj_size=sensor.getObjectSize();
+    switch(gesture){
+      case GES_UP:
+      strcpy(SentMessage,"U");
+      Serial.println("UP");
+      break;
+      case GES_DOWN:
+      strcpy(SentMessage,"D");
+      Serial.println("DOWN");
+      break;
+      case GES_LEFT:
+      strcpy(SentMessage,"L");
+      Serial.println("LEFT");
+      break;
+      case GES_RIGHT:
+      strcpy(SentMessage,"R");
+      Serial.println("RIGHT");
+      break;
+      case GES_FORWARD:
+      strcpy(SentMessage,"S");
+        Serial.println("FORWARD");
+      break;
+      case GES_BACKWARD:
+      strcpy(SentMessage,"S");
+        Serial.println("BACKWARD");
+      break;
+      case GES_CLOCKWISE:
+      strcpy(SentMessage,"C");
+      Serial.println("CLOCKWISE");
+      break;
+      case GES_ANTICLOCKWISE:
+      strcpy(SentMessage,"V");
+      Serial.println("ANTICLOCKWISE");
+      break;
+      case GES_WAVE:
+      Serial.println("Wave");
+      break;
+      case GES_NONE:
+      strcpy(SentMessage," ");
+      break;
+    }
+}
+//-----------------------------------
+//void curMode(){
+//  if(sensor.isCursorInView()){
+//    Serial.print("X: ");
+//    Serial.print(sensor.getCursorX());
+//    Serial.print(" Y: ");
+//    Serial.println(sensor.getCursorY());
+//  }
+//}
+//void proxMode(int obj){
+//    if(obj<300) strcpy(SentMessage,"1");
+//    if(obj>=300 && obj<800) strcpy(SentMessage,"2");
+//    if(obj>=800) strcpy(SentMessage,"3");
+//    Serial.print("Size: ");
+//    Serial.println(obj);
+//}
+
+void setup() {
+  Serial.begin(9600); //Set baud rate for virtual terminal 
+  sensor.begin(); //Start the sensor
+  sensor.setNormalSpeed();//Set normal scan speed (60-600o)
+  sensor.setGestureMode();//Set mode to Gesture mode
+  sensor.setGestureEntryTime(0);//The wait time before a gesture is process
+  sensor.setGestureExitTime(200);//The wait time before receiving a new gesture
+    //------------------
+
+    radio.begin();//Start the transceiver
+  radio.openWritingPipe(addr);// Open the pipe for writing, choose pipe 0 be default
+  radio.setPALevel(RF24_PA_MIN); //Set power level
+  radio.setChannel(108); // Set channel, choose one in 0-125, , must be identical to the receiver
+  radio.setDataRate(RF24_250KBPS); //Set data transfer rate
+  radio.stopListening();//Set the transceiver as the transmitter
+  if (!radio.available())
+  {
+    Serial.println("Connecting the RX.......");
+  }
+}
+void loop() {
+      gesMode();
+      if(SentMessage[0]!=' '){
+        radio.write(&SentMessage,sizeof(SentMessage)); // Write the data to the pipe and start sending the package
+      }
+}
---
+++ RC_receiver.ino
@@ -0,0 +1,125 @@
+#include <AFMotor.h>
+#include <SPI.h>
+#include <Arduino.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+AF_DCMotor motor1(1); 
+AF_DCMotor motor2(2); 
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+RF24 radio(9,10); //Create a transceiver object that has CE and CSE pins at 9 and 10
+const byte addr[6]="12345";//Tranmission address, must be identical to the transmitter
+char Message[2];//Initialize a char array for storing data
+uint8_t motorSpeed[]={100,100,100,100}; // Initialize the motor speed(0-255)
+
+//-----------------Transform the command to motor control-----------
+void stopMotor(){
+    motor1.run(RELEASE);
+    motor1.setSpeed(0); 
+    motor2.run(RELEASE);
+    motor2.setSpeed(0); 
+    motor3.run(RELEASE); 
+    motor3.setSpeed(0); 
+    motor4.run(RELEASE); 
+    motor4.setSpeed(0);
+    delay(500);
+}
+void checkCmd(){
+  if(Message[0]=='U'){
+    motor1.run(FORWARD);
+    motor1.setSpeed(motorSpeed[0]); 
+    motor2.run(FORWARD);
+    motor2.setSpeed(motorSpeed[1]); 
+    motor3.run(FORWARD); 
+    motor3.setSpeed(motorSpeed[2]); 
+    motor4.run(FORWARD); 
+    motor4.setSpeed(motorSpeed[3]); 
+  }
+  if(Message[0]=='D'){
+    motor1.run(BACKWARD); 
+    motor1.setSpeed(motorSpeed[0]); 
+    motor2.run(BACKWARD); 
+    motor2.setSpeed(motorSpeed[1]); 
+    motor3.run(BACKWARD); 
+    motor3.setSpeed(motorSpeed[2]); 
+    motor4.run(BACKWARD); 
+    motor4.setSpeed(motorSpeed[3]); 
+  }
+  if(Message[0]=='L'){
+    motor1.run(FORWARD);
+    motor1.setSpeed(motorSpeed[0]); 
+    motor2.run(BACKWARD); 
+    motor2.setSpeed(motorSpeed[1]); 
+    motor3.run(FORWARD);  
+    motor3.setSpeed(motorSpeed[2]); 
+    motor4.run(BACKWARD);
+    motor4.setSpeed(motorSpeed[3]); 
+  }
+  if(Message[0]=='R'){
+    motor1.run(BACKWARD); 
+    motor1.setSpeed(motorSpeed[0]); 
+    motor2.run(FORWARD); 
+    motor2.setSpeed(motorSpeed[1]); 
+    motor3.run(BACKWARD);
+    motor3.setSpeed(motorSpeed[2]); 
+    motor4.run(FORWARD); 
+    motor4.setSpeed(motorSpeed[3]);
+  }
+  if(Message[0]=='1'){
+    
+  }
+  if(Message[0]=='2'){
+    
+  }
+  if(Message[0]=='3'){
+    
+  }
+  if(Message[0]=='C'){
+    motor1.run(FORWARD);
+    motor1.setSpeed(motorSpeed[0]); 
+    motor2.run(BACKWARD); 
+    motor2.setSpeed(motorSpeed[1]); 
+    motor3.run(BACKWARD);  
+    motor3.setSpeed(motorSpeed[2]); 
+    motor4.run(FORWARD);
+    motor4.setSpeed(motorSpeed[3]); 
+  }
+  if(Message[0]=='V'){
+    motor1.run(BACKWARD);
+    motor1.setSpeed(motorSpeed[0]); 
+    motor2.run(FORWARD); 
+    motor2.setSpeed(motorSpeed[1]); 
+    motor3.run(FORWARD);  
+    motor3.setSpeed(motorSpeed[2]); 
+    motor4.run(BACKWARD);
+    motor4.setSpeed(motorSpeed[3]);
+  }
+}
+//--------------------------------------------------------
+
+void setup() {
+  Serial.begin(9600);//Set baud rate for virtual terminal 
+  radio.begin();//Start the transceiver
+  radio.openReadingPipe(1, addr);// Open pipe 1 for reading, choose pipe from 1-5 (pipe 0 is for writing)
+  radio.setPALevel(RF24_PA_MIN);// Set power level
+  radio.setChannel(108);//Set channel, choose one in 0-125, must be identical to the transmitter
+  radio.setDataRate(RF24_250KBPS);//Set data transfer rate
+  radio.startListening();//Set the transceiver as the transmitter
+  if (!radio.available())
+  {
+    Serial.println("Connecting the TX.......");
+  }
+
+}
+void loop() { 
+  if (radio.available()) {//Start reading if the transceivers are connected
+      radio.read(&Message, sizeof(Message));// Read the data from the pipe and savee it to the char array
+      if(Message[0]!=' '){
+        Serial.println(Message);
+            stopMotor();//Stop all motors before processing another command
+          checkCmd();//Process the command
+      }
+    }
+}
